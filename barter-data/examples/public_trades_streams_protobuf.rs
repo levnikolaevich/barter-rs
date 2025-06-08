1c1ef12a2308995dd64cd446139b9ba5dc96bd0e
levnikolaevich <levnikolaevich@ya.ru>
2025-06-08 17:17:09 +0200

    FIX

--- barter-data/examples/public_trades_streams_protobuf.rs
+++ barter-data/examples/public_trades_streams_protobuf.rs
@@ -41,7 +41,7 @@ fn init_logging() {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::filter::EnvFilter::builder()
-                .with_default_directive(tracing_subscriber::filter::LevelFilter::DEBUG.into())
+                .with_default_directive(tracing_subscriber::filter::LevelFilter::INFO.into())
                 .from_env_lossy(),
         )
         .with_ansi(cfg!(debug_assertions))
